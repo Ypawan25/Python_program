583afd358de3651681da46fcd69d4dbec546f326
Ypawan25 <impawakryadav@gmail.com>
2023-01-04 19:18:18 +0530

    for loop in python

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+
+for i in range(7):
+    print(i)
+print("**********************************")
+
+arr = [1,2,3,4,5,6,7,8]
+
+for k in arr:
+    if(k%2==0):
+        print("Even")
+    else:
+        print("Odd")    
+print("*********************")
+
